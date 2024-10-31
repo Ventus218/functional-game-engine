39811f0f50ce3f17125a6acdae0d31d3283435e3
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-10-31 10:10:24 +0100

    More concise Trampoline

--- src/main/scala/monads/Trampoline.scala
+++ src/main/scala/monads/Trampoline.scala
@@ -17,14 +17,12 @@ object Trampoline:
 
   given Monad[Trampoline] with
     def unit[A](a: => A): Trampoline[A] =
-      More(() => Done(a))
+      Done(a)
 
     extension [A](m: Trampoline[A])
       def flatMap[B](f: A => Trampoline[B]): Trampoline[B] = m match
         case Done(value) => f(value)
         case More(work)  => More(() => work().flatMap(f))
-
-      override def map[B](f: A => B): Trampoline[B] = m.flatMap(a => Done(f(a)))
 
   extension [A](t: Trampoline[A])
     @tailrec
@@ -49,4 +47,4 @@ object TestTrampoline extends App:
         case None => done(List())
 
   // println(List.fill(5000)("s").myZipIndex())
-  println(List.fill(20000)("s").myZipIndexT().evaluate())
+  println(List.fill(50000)("s").myZipIndexT().evaluate())
