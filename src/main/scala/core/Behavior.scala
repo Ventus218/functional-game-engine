8c886e8cb81bcb92f182664660e878c086077e12
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-11-04 19:45:17 +0100

    Added methods that will be required for Behavior

--- src/main/scala/core/Behavior.scala
+++ src/main/scala/core/Behavior.scala
@@ -5,7 +5,7 @@ import cats.effect.IO
 import Engine.*
 
 object Behavior:
-  trait Behavior(val enabled: Boolean = true):
+  trait Behavior(/*val enabled: Boolean = true*/):
 
     /** Called only once when the behavior is instatiated. */
     def onInit(selfId: String): StateT[IO, Engine, Unit] = StateT.empty
@@ -25,3 +25,14 @@ object Behavior:
     /** Called only once before the engine destroys the object. */
     def onDeinit(selfId: String): StateT[IO, Engine, Unit] = StateT.empty
 
+    /** Called:
+      *   - after the behavior is instantiated as enabled
+      *   - every time the behavior goes from disabled to enabled.
+      */
+    // def onEnabled(selfId: String): StateT[IO, Engine, Unit] = StateT.empty
+
+    /** Called:
+      *   - after the behavior is instantiated as disabled
+      *   - every time the behavior goes from enabled to disabled.
+      */
+    // def onDisabled(selfId: String): StateT[IO, Engine, Unit] = StateT.empty
